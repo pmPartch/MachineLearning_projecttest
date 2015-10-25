75cc0473d11725205ab07bb6954d8facd02ba2e5
pmPartch <pmp@pmconsult.com>
2015-10-25 14:51:46 -0700

    last minute updates from remote server tests

--- Project Remote.R
+++ Project Remote.R
@@ -10,11 +10,16 @@ trainnacols <- traingdata[,colSums(is.na(traingdata))>nrow(traingdata)/10]
 #find index of columns with high counts of NA's
 naIndexs <- which(names(traingdata) %in% names(trainnacols))
 
-#I'm also going to remove unused columns (X, user_name, time stamps and window: column indexs: 1:7)
-naIndexs <- append(1:7, naIndexs)
+#I'm also going to remove unused columns (X, user_name, time stamps and window: column indexs: 1:6)
+naIndexs <- append(1:6, naIndexs)
 
 #now remove these columns from the dataset
 cleandata <- traingdata[,-naIndexs]
+
+#also try to reduce to minimum features with max variablity (plus num_window)
+savenames <- c("num_window","roll_belt","pitch_belt","yaw_belt","roll_arm","pitch_arm","yaw_arm","roll_forearm","pitch_forearm","yaw_forearm","classe")
+useIndexs <- which(names(cleandata) %in% savenames)
+cleandata2 <- cleandata[,useIndexs]
 
 if (require("caret") == FALSE)
 {
@@ -27,6 +32,18 @@ inTrain <- createDataPartition(y=cleandata$classe, p=0.6, list=FALSE)
 
 cleantraining <- cleandata[inTrain,]
 cleantesting <- cleandata[-inTrain,]
+
+#also split the reduced data as well for futher testing
+
+library(caret)
+
+set.seed(12345)
+inTrain2 <- createDataPartition(y=cleandata2$classe, p=0.6, list=FALSE)
+
+cleantraining2 <- cleandata2[inTrain2,]
+cleantesting2 <- cleandata2[-inTrain2,]
+
+#now do model fitting
 
 set.seed(5436)
 
@@ -48,7 +65,7 @@ if (require("doParallel") == FALSE)
   library(doParallel)
 }
 #cl <- makeCluster(10) #setup run to use 10 cores
-registerDoParallel(cores = 12)
+registerDoParallel(cores = 16)
 
 #standard default train setup
 set.seed(4532)
@@ -58,6 +75,15 @@ system.time(
 modFit
 saveRDS(modFit,"rfFullModel.rds")
 rm(modFit)
+
+#standard default with tweak to tuneLength from 3 to 6
+set.seed(4532)
+system.time(
+  modFitb <- train(classe ~ .,data=cleantraining,method="rf",prox=TRUE, tuneLength=6)
+)
+modFitb
+saveRDS(modFitb,"rfFullBModel.rds")
+rm(modFitb)
 
 
 #change to repeated cv with repeats of 5
@@ -72,6 +98,35 @@ system.time(
 modFit2
 saveRDS(modFit2,"rfFullModel2.rds")
 rm(modFit2)
+
+#change to repeated cv with repeats of 5 and tuneLength=6
+set.seed(4532)
+ctrl <- trainControl(method = "repeatedcv", repeats = 5)
+
+
+system.time(
+  modFit2b <- train(classe ~ ., data = cleantraining, method = "rf",prox=TRUE, trControl = ctrl, tuneLength=6)
+)
+
+modFit2b
+saveRDS(modFit2b,"rfFullModel2b.rds")
+rm(modFit2b)
+
+####
+#change to repeated cv with repeats of 5 and tuneLength=6 and use reduced data
+set.seed(4532)
+ctrl <- trainControl(method = "repeatedcv", repeats = 5)
+
+
+system.time(
+  modFit2b2 <- train(classe ~ ., data = cleantraining2, method = "rf",prox=TRUE, trControl = ctrl, tuneLength=6) # 5124.727 secs to train to get 99.8% accuracy 
+)
+
+modFit2b2
+saveRDS(modFit2b2,"rfFullModel2b2.rds")
+rm(modFit2b2)
+
+####
 
 #change performance metrics (note, twoclass summary is not appropirate for 5 outputs)
 set.seed(4532)
